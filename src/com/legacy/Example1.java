4fdd8735dac3107714d349db929e2d544f971152
MidhunBalan <midhun.balan@anywhere.co>
2021-03-07 09:22:47 +0530

    sorting example

--- src/com/legacy/Example1.java
+++ src/com/legacy/Example1.java
@@ -2,6 +2,7 @@ package com.legacy;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 
 public class Example1 {
 
@@ -15,6 +16,24 @@ public class Example1 {
 		System.out.println("Before sorting:"+list);
 		Collections.sort(list);
 		System.out.println("After sorting:"+list);	
+		
+		Collections.sort(list, new MayComparator());
+		System.out.println("After custom sorting(Descending order):"+list);	
 	}
 
+}
+
+class MayComparator implements Comparator<Integer>{
+
+	@Override
+	public int compare(Integer ob1, Integer ob2) {
+		if(ob1> ob2) {
+			return -1;
+		}else if(ob1< ob2) {
+			return 1;
+		}else {
+			return 0;
+		}
+	}
+	
 }
