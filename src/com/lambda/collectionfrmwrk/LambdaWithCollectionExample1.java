3dc666329e2c417dd13a79113aa759f118124663
MidhunBalan <midhun.balan@anywhere.co>
2021-03-07 10:46:49 +0530

    comparatore with java

--- src/com/lambda/collectionfrmwrk/LambdaWithCollectionExample1.java
+++ src/com/lambda/collectionfrmwrk/LambdaWithCollectionExample1.java
@@ -1,5 +1,26 @@
 package com.lambda.collectionfrmwrk;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
+
+/*
+ * Program with comparator and lambda
+ */
 public class LambdaWithCollectionExample1 {
+	public static void main(String arg[]) {
+		ArrayList<Integer> list = new ArrayList<>();
+		list.add(100);
+		list.add(5);
+		list.add(200);
+		list.add(110);
+		list.add(300);
+		System.out.println("Before sorting:"+list);
+		Collections.sort(list);
+		System.out.println("After sorting:"+list);	
+		
+		Collections.sort(list, (Obj1, Obj2) -> (Obj1 > Obj2)?-1: (Obj1 < Obj2)? 1: 0 );
+		System.out.println("After custom sorting(Descending order):"+list);	
+	}
 
 }
